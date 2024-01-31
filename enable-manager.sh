d0f58321f363a4390ded8630f5e6ced7031c9ca8
prabhatraghav <156128444+prabhatraghav@users.noreply.github.com>
2024-01-31 22:20:26 +0530

    Update enable-manager.sh

--- enable-manager.sh
+++ enable-manager.sh
@@ -1,19 +1,19 @@
 cd /opt/tomcat/webapps/manager/META-INF
 mv context.xml backup.context.xml
-wget https://raw.githubusercontent.com/prabhatraghav/tomcat-install/main/manager.xml
+wget https://raw.githubusercontent.com/prabhatraghav/tomcat-install/main/context.xml/manager.xml
 mv manager.xml context.xml
 
 cd /opt/tomcat/webapps/host-manager/META-INF
 mv context.xml backup.context.xml
-wget https://raw.githubusercontent.com/prabhatraghav/tomcat-install/main/host-manager.xml
+wget https://raw.githubusercontent.com/prabhatraghav/tomcat-install/main/context.xml/host-manager.xml
 mv host-manager.xml context.xml
 
 cd /opt/tomcat/webapps/examples/META-INF
 mv context.xml backup.context.xml
-wget https://raw.githubusercontent.com/prabhatraghav/tomcat-install/main/examples.xml
+wget https://raw.githubusercontent.com/prabhatraghav/tomcat-install/main/context.xml/examples.xml
 mv examples.xml context.xml
 
 cd /opt/tomcat/webapps/docs/META-INF
 mv context.xml backup.context.xml
-wget https://raw.githubusercontent.com/prabhatraghav/tomcat-install/main/docs.xml
+wget https://raw.githubusercontent.com/prabhatraghav/tomcat-install/main/context.xml/docs.xml
 mv docs.xml context.xml
