75427085f1cadce7ad99331cba0970a128934636
prabhatraghav <156128444+prabhatraghav@users.noreply.github.com>
2024-02-02 00:27:41 +0530

    Update tomcat-install.sh

--- tomcat-install.sh
+++ tomcat-install.sh
@@ -83,6 +83,24 @@ sudo chmod +x /usr/local/bin/tomcat-down
 # Clean up
 rm /tmp/apache-tomcat-${TOMCAT_VERSION}.tar.gz
 
+# To enable Tomcat Manger, replacing the context.xml files
+cd /opt/tomcat/webapps/manager/META-INF
+mv context.xml backup.context.xml
+wget https://raw.githubusercontent.com/prabhatraghav/tomcat-install/main/context.xml/manager.xml
+mv manager.xml context.xml
+cd /opt/tomcat/webapps/host-manager/META-INF
+mv context.xml backup.context.xml
+wget https://raw.githubusercontent.com/prabhatraghav/tomcat-install/main/context.xml/host-manager.xml
+mv host-manager.xml context.xml
+cd /opt/tomcat/webapps/examples/META-INF
+mv context.xml backup.context.xml
+wget https://raw.githubusercontent.com/prabhatraghav/tomcat-install/main/context.xml/examples.xml
+mv examples.xml context.xml
+cd /opt/tomcat/webapps/docs/META-INF
+mv context.xml backup.context.xml
+wget https://raw.githubusercontent.com/prabhatraghav/tomcat-install/main/context.xml/docs.xml
+mv docs.xml context.xml
+
 # Print instructions
 echo "Apache Tomcat $TOMCAT_VERSION has been installed to $TOMCAT_DIR"
 echo "Tomcat Manager credentials:"
