0c7ff001eec3acd0d4d57008d664d0a90e40a5a7
Prabhat Singh Raghav <156128444+prabhatraghav@users.noreply.github.com>
2024-02-13 14:33:30 +0530

    Update tomcat-install.sh

--- tomcat-install.sh
+++ tomcat-install.sh
@@ -11,7 +11,7 @@ TOMCAT_MANAGER_USER="admin"
 TOMCAT_MANAGER_PASS="admin@1"  # Change this to a secure password
 CUSTOM_TOMCAT_PORT="8080"       # Leave empty to use default port (8080)
 CUSTOM_JDK_VERSION="17"       # Leave empty to use default JDK
-PUBLIC_IP_ADDR='curl -s http://whatismyip.akamai.com/'
+PUBLIC_IP_ADDR=`curl -s http://whatismyip.akamai.com/`
 LINE_BORDER_EQ="====================================================================="
 LINE_BORDER_SLASH="|||///|||\\\|||///|||\\\|||///|||\\\|||///|||\\\|||///|||\\\|||///|||\\\|||"
 LINE_BORDER_HASH="######################################################################"
