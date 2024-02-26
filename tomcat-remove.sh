6863deba77189ecc2227699099b8412a03fcfd89
Prabhat Singh Raghav <156128444+prabhatraghav@users.noreply.github.com>
2024-02-26 18:24:00 +0530

    Update tomcat-remove.sh

--- tomcat-remove.sh
+++ tomcat-remove.sh
@@ -2,9 +2,7 @@
 sudo tomcat-down
 sleep 10
 sudo rm -r /opt/tomcat/
-cd /usr/local/bin/
-sudo rm -r tomcat-up
-sudo rm -r tomcat-down
+sudo rm -r /usr/local/bin/tomcat-up
+sudo rm -r /usr/local/bin/tomcat-down
 echo "Tomact removed successfully from the machine"
-cd /home
 sudo rm -r tomcat-remove.sh
